c0f38141451674d05a856bc7398749beee7f5f0c
lennin-developer <lennin.developer@gmail.com>
2024-06-12 03:32:28 -0400

    Creating sub components for each section of the page

--- InvoiceApp/src/app/components/invoice/invoice.component.ts
+++ InvoiceApp/src/app/components/invoice/invoice.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { InvoiceService } from '../../services/invoice.service';
 import { Invoice } from '../../models/invoice';
+import { CompanyViewComponent } from '../company-view/company-view.component';
+import { RowItemsComponent } from '../row-item/row-item.component';
+import { ListItemsComponent } from '../list-items/list-items.component';
+import { ClientViewComponent } from '../client-view/client-view.component';
+import { InvoiceViewComponent } from '../invoice-view/invoice-view.component';
 
 @Component({
   selector: 'app-invoice',
   standalone: true,
-  imports: [],
-  templateUrl: './invoice.component.html'
+  imports: [
+    RowItemsComponent,
+    ListItemsComponent,
+    CompanyViewComponent,
+    ClientViewComponent,
+    InvoiceViewComponent,
+  ],
+  templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit {
-
   invoice!: Invoice;
 
-  constructor(private service: InvoiceService){}
+  constructor(private service: InvoiceService) {}
 
   ngOnInit(): void {
     this.invoice = this.service.getInvoice();
