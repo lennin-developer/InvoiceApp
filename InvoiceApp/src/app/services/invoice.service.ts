53b316a88a6e717e4189f50ff6238fc076fde67d
lennin-developer <lennin.developer@gmail.com>
2024-06-12 00:02:43 -0400

    Displaying the invoice data inside invoice component html

--- InvoiceApp/src/app/services/invoice.service.ts
+++ InvoiceApp/src/app/services/invoice.service.ts
@@ -5,7 +5,7 @@ import { Invoice } from '../models/invoice';
 @Injectable({
   providedIn: 'root'
 })
-export class ItemService {
+export class InvoiceService {
   private invoice: Invoice = invoiceData;
   constructor() { }
 
