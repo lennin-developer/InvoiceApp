4689b11cd3a3cbe72c33cfa0cbaf8410ea3e7e4c
lennin-developer <lennin.developer@gmail.com>
2024-06-13 16:29:32 -0400

    removing item from page invoice

--- InvoiceApp/src/app/components/list-items/list-items.component.ts
+++ InvoiceApp/src/app/components/list-items/list-items.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Item } from '../../models/items';
 import { RowItemComponent } from '../row-item/row-item.component';
 
@@ -10,4 +10,9 @@ import { RowItemComponent } from '../row-item/row-item.component';
 })
 export class ListItemsComponent {
   @Input() items: Item[] = [];
+
+  @Output() removeEventEmitter: EventEmitter<number> = new EventEmitter();
+  onRemove(id: number) {
+    this.removeEventEmitter.emit(id);
+  }
 }
--- InvoiceApp/src/app/components/row-item/row-item.component.ts
+++ InvoiceApp/src/app/components/row-item/row-item.component.ts
@@ -1,12 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Item } from '../../models/items';
 
 @Component({
-  selector: 'tr[row-item]',//agregamos tr para que se muestre en una fila
+  selector: 'tr[row-item]', //agregamos tr para que se muestre en una fila
   standalone: true,
   imports: [],
   templateUrl: './row-item.component.html',
 })
 export class RowItemComponent {
   @Input() item!: Item;
+
+  @Output() removeEventEmitter: EventEmitter<number> =
+    new EventEmitter<number>();
+
+  onRemove(id: number) {
+    this.removeEventEmitter.emit(id);
+  }
 }
+
+
