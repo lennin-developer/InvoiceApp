c012f6199b58113e19130c1769cec9e850597949
lennin-developer <lennin.developer@gmail.com>
2024-06-13 15:45:58 -0400

    Add total ammount and use reduce function (similar stream api)

--- InvoiceApp/src/app/services/invoice.service.ts
+++ InvoiceApp/src/app/services/invoice.service.ts
@@ -7,10 +7,22 @@ import { Invoice } from '../models/invoice';
 })
 export class InvoiceService {
   private invoice: Invoice = invoiceData;
+
   constructor() { }
 
   getInvoice(): Invoice{
-    return this.invoice;
+    const total = this.calculateTotal();
+    return {... this.invoice, total};
+  }
+
+  calculateTotal(){
+    // let total = 0;
+    // this.invoice.items.forEach(item => {
+    //   total += item.total();
+    // });
+    // return total;
+    //lo mismo de arriba pero con reduce
+    return this.invoice.items.reduce((total, item) => total + item.total(), 0);
   }
 
 }
